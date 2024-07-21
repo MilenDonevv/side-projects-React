cc6495cd9a3db03740958dfe3c93c83b2dc61d75
MilenDonevv <milendonev95@gmail.com>
2024-07-21 16:48:12 +0300

    set func to open diff options

--- Accordion/src/components/accordion/index.jsx
+++ Accordion/src/components/accordion/index.jsx
@@ -1,32 +1,40 @@
+/* eslint-disable react/jsx-key */
 // single selection
 // multiple selection
 
 import { useState } from "react";
 import data from "./data.js";
 
-
-
 export default function Accordion() {
+  const [selected, setSelected] = useState(null);
 
-    const [selected, setSelected] = useState(null);
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId);
+  }
 
-
-    return (
-        <div className="wrapper">
-            <div className="accordion">
-            {
-                data && data.length > 0 ? 
-                data.map(dataItem => 
-                <div className="item">
-                    <div className="title">
-                        <h3>{dataItem.question}</h3>
-                        <span>+</span>
-                    </div>
-                </div>)
-                
-                : <div>No data found!</div>
-            }
+  console.log(selected);
+  return (
+    <div className="wrapper">
+      <div className="accordion">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div
+                onClick={() => handleSingleSelection(dataItem.id)}
+                className="title"
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {selected === dataItem.id ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null}
             </div>
-        </div>
-    )
+          ))
+        ) : (
+          <div>No data found!</div>
+        )}
+      </div>
+    </div>
+  );
 }
