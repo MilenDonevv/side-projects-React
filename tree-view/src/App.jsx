0a9bfe819616cb7b72f93232dd534841ab756e6f
MilenDonevv <milendonev95@gmail.com>
2024-07-31 19:20:21 +0300

    add basic menu options

--- tree-view/src/App.jsx
+++ tree-view/src/App.jsx
@@ -1,8 +1,11 @@
+import TreeView from "./index.jsx"
+import menus from "./data.js"
+
 function App() {
 
   return (
     <div>
-      
+      <TreeView menus={menus} />
     </div>
   )
 }
--- tree-view/src/menu-item.jsx
+++ tree-view/src/menu-item.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+import MenuList from "./menu-list";
 
 
 export default function MenuItem({item}) {
-
+    const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
+    
     return (
-        <div>
-            Menu item!
-        </div>
+        <li>
+            <div style={{display: 'flex', gap: '20px'}}> 
+            <p>{item.label}</p>
+            {
+                item && item.children && item.children.length ? <span>+</span> : null
+            }
+            </div>
+
+            {
+                item && item.children && item.children.length > 0 ? (
+                <MenuList list={item.children} /> )
+                : null
+            }
+        </li>
     )
 }
+
