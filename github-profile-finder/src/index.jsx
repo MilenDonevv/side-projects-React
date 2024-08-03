84511fdb46de8e12d03a5b39dbde93c6c52d4432
MilenDonevv <milendonev95@gmail.com>
2024-08-03 12:05:12 +0300

    finish functionality with github API

--- github-profile-finder/src/index.jsx
+++ github-profile-finder/src/index.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import User from "./user";
 
 
 
@@ -6,24 +7,44 @@ import { useEffect, useState } from "react"
 export default function GithubProfileFinder() {
 
     const [userName, setUserName] = useState('MilenDonevv')
+    const [userData, setUserData] = useState(null);
+    const [loading, setLoading] = useState(true);
 
 
 
+
+
+    
+    
+    async function fetchGithubUserData() {
+        setLoading(true);
+        
+        const res = await fetch(`https://api.github.com/users/${userName}`);
+        const data = await res.json();
+        
+        if (data) {
+            setUserData(data);
+            setLoading(false);
+            setUserName('')
+        }
+        
+        console.log(data);
+    }
+    
     function handleSubmit() {
-
+        fetchGithubUserData();
     }
-
-
-    function fetchGithubUserData() {
-
-
-    }
-
-
+    
     
     useEffect(() => {
         fetchGithubUserData()
     }, []);
+
+    if (loading) {
+        return <h1>Loading data! Please wait!</h1>
+    }
+
+
 
     return (
         <div className="github-profile-container">
@@ -32,12 +53,17 @@ export default function GithubProfileFinder() {
                 name="search-by-username"
                 type="text" 
                 placeholder="Search Github Username..."
+                value={userName}
+                onChange={(event) => setUserName(event.target.value)}
                 />
 
                 <button onClick={handleSubmit}>
                     Search
                 </button>
             </div>
+            {
+                userData !== null ? <User user={userData} /> : null
+            }
         </div>
     )
 }
