995d95d403927b9a0d3b30032bcbce81ff9a6ddc
MilenDonevv <milendonev95@gmail.com>
2024-08-03 17:07:23 +0300

    initial setup

---
+++ tic-tac-toe/src/index.jsx
@@ -0,0 +1,32 @@
+
+// 0 1 2
+// 3 4 5
+// 6 7 8
+
+
+function Square({ value }) {
+
+    return (
+        <button>
+            {value}
+        </button>
+    )
+}
+
+export default function TicTacToe() {
+
+
+    return (
+        <div>
+            <div className="row">
+
+            </div>
+            <div className="row">
+
+            </div>
+            <div className="row">
+
+            </div>
+        </div>
+    )
+}
