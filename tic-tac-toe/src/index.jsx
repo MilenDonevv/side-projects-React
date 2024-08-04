cd93864879378029d7ad86bf9cf68e8c6afd6a33
MilenDonevv <milendonev95@gmail.com>
2024-08-04 19:40:11 +0300

    add restart btn & finish app

--- tic-tac-toe/src/index.jsx
+++ tic-tac-toe/src/index.jsx
@@ -1,13 +1,15 @@
+import { useState } from 'react'
 import './styles.css'
+import { useEffect } from 'react';
 // 0 1 2
 // 3 4 5
 // 6 7 8
 
 
-function Square({ value }) {
+function Square({ value, onClick }) {
 
     return (
-        <button className="square">
+        <button onClick={onClick} className="square">
             {value}
         </button>
     )
@@ -15,24 +17,78 @@ function Square({ value }) {
 
 export default function TicTacToe() {
 
+    const [squares, setSquares] = useState(Array(9).fill(''));
+    const [isXturn, setIsXTurn] = useState(true);
+    const [status, setStatus] = useState('')
+
+    function handleClick(getCurrentSquare) {
+        let cpySquares = [...squares];
+        if (getWinner(squares) || cpySquares[getCurrentSquare]) return;
+        cpySquares[getCurrentSquare] = isXturn ? "X" : "O";
+        setIsXTurn(!isXturn);
+        setSquares(cpySquares);
+    }
+
+    function handleRestart() {
+        setIsXTurn(true);
+        setSquares(Array(9).fill(''))
+    }
+
+    function getWinner(squares) {
+        const winningPatterns = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6],
+            [0,3,6],
+            [1,4,7],
+        ];
+
+        for (let i = 0; i < winningPatterns.length; i++) {
+            const [x,y,z] = winningPatterns[i];      // x, y, z = 012 || 345 || 678 || ...
+
+            if(squares[x] && squares[x] === squares[y] && squares[x] === squares[z]) {
+                return squares[x];
+            }
+        }
+
+        return null;
+    }
+
+    useEffect(() => {
+        if (!getWinner(squares) && squares.every(square => square !== '')) {
+            setStatus(`The game is Draw! Please restart the game!`);
+        } else if (getWinner(squares)) {
+            setStatus(`Winner is ${getWinner(squares)}`)
+        } else {
+            setStatus(`Next player's turn now is ${isXturn ? 'X' : 'O'}`)
+        }
+
+    }, [squares, isXturn])
+
+    console.log(squares)
 
     return (
         <div className="tic-tac-toe-container">
             <div className="row">
-                <Square />
-                <Square />
-                <Square />
+                <Square value={squares[0]} onClick={() => handleClick(0)} />
+                <Square value={squares[1]} onClick={() => handleClick(1)} />
+                <Square value={squares[2]} onClick={() => handleClick(2)} />
             </div>
             <div className="row">
-                <Square />
-                <Square />
-                <Square />
+                <Square value={squares[3]} onClick={() => handleClick(3)} />
+                <Square value={squares[4]} onClick={() => handleClick(4)} />
+                <Square value={squares[5]} onClick={() => handleClick(5)} />
             </div>
             <div className="row">
-                <Square />
-                <Square />
-                <Square />
+                <Square value={squares[6]} onClick={() => handleClick(6)} />
+                <Square value={squares[7]} onClick={() => handleClick(7)} />
+                <Square value={squares[8]} onClick={() => handleClick(8)} />
             </div>
+            <h1>{status}</h1>
+            <button onClick={handleRestart}>Restart</button>
         </div>
     )
 }
