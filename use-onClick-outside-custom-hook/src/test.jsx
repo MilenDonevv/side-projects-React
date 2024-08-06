7da0ba46bbcf27b28dc3c8ac89d74d3b3fb8bee9
MilenDonevv <milendonev95@gmail.com>
2024-08-06 15:01:19 +0300

    finish hook customization

--- use-onClick-outside-custom-hook/src/test.jsx
+++ use-onClick-outside-custom-hook/src/test.jsx
@@ -1,17 +1,22 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
+import useOutsideClick from "./index.jsx";
 
 
 
 
 export default function UseOnclickOutsideTest() {
-
     const [showContent, setShowContent] = useState(false);
+    const ref = useRef();
+
+
+    useOutsideClick(ref, () => setShowContent(false))
+
 
     return (
         <div>
             {
                 showContent 
-                ? <div>
+                ? <div ref={ref}>
                     <h1>This is a random content</h1>
                     <p>Click outside of this to close it. Otherwise it will not close</p>
                 </div>
