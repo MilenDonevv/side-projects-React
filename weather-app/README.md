c6c87f6a513ce08549fbfec3407cfa16290dc032
MilenDonevv <milendonev95@gmail.com>
2024-08-07 21:56:31 +0300

    initial setup

---
+++ weather-app/README.md
@@ -0,0 +1,2 @@
+# This is an app to check the weather in different cities from different countries
+
---
+++ weather-app/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+
+  return (
+
+    <WeatherApp />
+  )
+}
+
+export default App
