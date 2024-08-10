ba1673b05ef0e2ede6a7ee2fda6f782869a8b762
MilenDonevv <milendonev95@gmail.com>
2024-08-10 17:47:28 +0300

    improve weather component

--- weather-app/src/index.jsx
+++ weather-app/src/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Search from "./search.jsx";
 
 
@@ -5,11 +6,40 @@ import Search from "./search.jsx";
 
 
 export default function Weather() {
+    const [search, setSearch] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [weatherData, setWeatherData] = useState(null);
+
+
+
+    async function handleSearch() {
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=d30104027345aff21a359fe29e432ebd`)
+
+            const data = await response.json();
+            console.log(data);
+
+
+            
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+
 
     return (
         <div>
-            <Search />
+            <Search 
+            search={search}
+            setSearch={setSearch}
+            handleSearch={handleSearch}
+            />
             Weather
         </div>
     )
 }
+
+
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+//  API key - d30104027345aff21a359fe29e432ebd
