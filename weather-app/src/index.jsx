bf4b97fb6183f63569904f2d5c026950c542e952
MilenDonevv <milendonev95@gmail.com>
2024-08-11 22:40:40 +0300

    API test working fine

--- weather-app/src/index.jsx
+++ weather-app/src/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Search from "./search.jsx";
 
 
@@ -6,28 +6,48 @@ import Search from "./search.jsx";
 
 
 export default function Weather() {
-    const [search, setSearch] = useState('');
+    const [search, setSearch] = useState('Chirpan');
     const [loading, setLoading] = useState(false);
     const [weatherData, setWeatherData] = useState(null);
 
 
+    async function fetchWeatherData(param) {
 
-    async function handleSearch() {
+        if (!param) {
+            console.log('Search term is empty!')
+            return;  // Prevent fetching if search term is empty
+        }
+
+        setLoading(true);
+
         try {
             const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=d30104027345aff21a359fe29e432ebd`)
-
+    
             const data = await response.json();
-            console.log(data);
-
-
+            
+            if(data) {
+                setWeatherData(data);
+                setLoading(false);
+            }
+    
             
         } catch (error) {
+            setLoading(false);
             console.log(error);
             
         }
     }
+    
+    
+    async function handleSearch() {
+        fetchWeatherData(search);
+    }
+    
+    useEffect(() => {
+        fetchWeatherData('Chirpan');
+    }, [])
 
-
+    console.log(weatherData)
     return (
         <div>
             <Search 
