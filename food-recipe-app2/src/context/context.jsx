ef51cef183777015e9f9202d0602538695adc596
MilenDonevv <milendonev95@gmail.com>
2024-08-12 19:38:15 +0300

    add API for recipes

--- food-recipe-app2/src/context/context.jsx
+++ food-recipe-app2/src/context/context.jsx
@@ -9,15 +9,28 @@ export const GlobalContext = createContext(null);
 
 
 
-export default function GlobalState({children}) {  // the children are all the nested components
+export default function GlobalState({ children }) {  // the children are all the nested components
 
     const [searchParam, setSearchParam] = useState('');
 
+    async function handleSubmit(event) {
 
+        event.preventDefault()
+
+        try {
+            const response = await fetch(`https://forkify-api.herokuapp.com/api/search?q=${searchParam}`);
+            const data = await response.json();
+
+            console.log(data);
+
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
 
     return (
         <>
-        <GlobalContext.Provider value={{ searchParam, setSearchParam }}>{children}</GlobalContext.Provider>
+            <GlobalContext.Provider value={{ searchParam, setSearchParam, handleSubmit }}>{children}</GlobalContext.Provider>
         </>
     )
 }
