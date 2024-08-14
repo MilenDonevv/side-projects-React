fbc6e0b6bdb14c0b7fedc6799ae62e6de1ad3637
MilenDonevv <milendonev95@gmail.com>
2024-08-14 16:19:22 +0300

    improve home page - add API + loading spinner

--- shopping-cart/src/pages/home.jsx
+++ shopping-cart/src/pages/home.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+import { Circles } from 'react-loader-spinner'
 
 
 
@@ -7,9 +9,46 @@
 
 export default function Home() {
 
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    async function fetchListOfProducts() {
+        setLoading(true);
+
+        const response = await fetch(`https://fakestoreapi.com/products`);
+        const data = await response.json();
+
+        if(data) {
+            setLoading(false);
+            setProducts(data);
+        }
+
+
+        console.log(products);
+        
+    } 
+
+
+    useEffect(() => {
+        fetchListOfProducts();
+    },[])
+
+
     return (
         <div>
-            Home
+            {
+                loading 
+                ? <div className="min-h-screen w-full flex justify-center items-center">
+                    <Circles 
+                    height={'120'}
+                    width={'120'}
+                    color='rgb(127,29,29)'
+                    visible={true}
+                    />
+
+                </div>
+                : null
+            }
         </div>
     )
 }
