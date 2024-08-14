a51f6594b099abb016e7c9e93b8528a7e875762d
MilenDonevv <milendonev95@gmail.com>
2024-08-14 18:00:47 +0300

    finish items layout

--- shopping-cart/src/pages/home.jsx
+++ shopping-cart/src/pages/home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { Circles } from 'react-loader-spinner'
+import ProductTile from "../product-tile/index.jsx";
 
 
 
@@ -18,36 +19,42 @@ export default function Home() {
         const response = await fetch(`https://fakestoreapi.com/products`);
         const data = await response.json();
 
-        if(data) {
+        if (data) {
             setLoading(false);
             setProducts(data);
         }
 
-
-        console.log(products);
-        
-    } 
+    }
 
 
     useEffect(() => {
         fetchListOfProducts();
-    },[])
+    }, [])
 
-
+    console.log(products);
+    
     return (
         <div>
             {
-                loading 
-                ? <div className="min-h-screen w-full flex justify-center items-center">
-                    <Circles 
-                    height={'120'}
-                    width={'120'}
-                    color='rgb(127,29,29)'
-                    visible={true}
-                    />
+                loading
+                    ? <div className="min-h-screen w-full flex justify-center items-center">
+                        <Circles
+                            height={'120'}
+                            width={'120'}
+                            color='rgb(127,29,29)'
+                            visible={true}
+                        />
 
-                </div>
-                : null
+                    </div>
+                    : (
+                        <div className="min-h-[80vh] grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl mx-auto p-3">
+                            {
+                                products && products.length ?
+                                    products.map((productItem, index) => <ProductTile key={index} product={productItem} />) :
+                                    null
+                            }
+                        </div>
+                    )
             }
         </div>
     )
