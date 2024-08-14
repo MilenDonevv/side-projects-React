3bfc4a3559a7fd5211eb1318fd03bf10b408ba4f
MilenDonevv <milendonev95@gmail.com>
2024-08-14 18:40:00 +0300

    initial redux toolkit setup

---
+++ shopping-cart/src/store/slices/cart-slice.js
@@ -0,0 +1,20 @@
+// create slice
+// initialize initial state
+// slice -> name, mention initial state, actions
+
+
+import {createSlice} from '@reduxjs/toolkit'
+
+
+const initialState = [];
+
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+
+    }
+})
+
+export default cartSlice.reducer;
