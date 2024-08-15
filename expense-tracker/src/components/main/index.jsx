ee09ed5421e4d5341fb2105cb1d98ffb89389127
MilenDonevv <milendonev95@gmail.com>
2024-08-15 16:30:49 +0300

    add basic income & expense & chart

--- expense-tracker/src/components/main/index.jsx
+++ expense-tracker/src/components/main/index.jsx
@@ -1,10 +1,49 @@
+import { Button, Flex, Heading } from "@chakra-ui/react";
+import Summary from "../summary/summary";
+import ExpenseView from "../expense-view/expense";
 
 
 export default function Main() {
-
     return (
-        <div>
-            Main
-        </div>
-    )
+        <Flex
+            textAlign={'center'}
+            flexDirection={'column'}
+            pr={'5'}
+            pl={'5'}
+            w="full"
+            alignItems="center"
+        >
+            <Flex
+                w="full"
+                alignItems={'center'}
+                justifyContent={'space-between'}
+                mt={'12'}
+                mb={'8'}
+                flexDirection={['column', 'column', 'row', 'row']}
+            >
+                <Heading color={'blue.400'} mb={['4', '4', '0', '0']}>
+                    Expense Tracker
+                </Heading>
+                <Button
+                    bg={'blue.300'}
+                    color={'black'}
+                    ml={'4'}
+                    mt={['4', '4', '0', '0']}
+                >
+                    Add New Transaction
+                </Button>
+            </Flex>
+            <Summary mb={8} />
+            <Flex
+                w={'full'}
+                alignItems={"flex-start"}
+                justifyContent={"space-between"}
+                flexDirection={['column', 'column', 'row', 'row']}
+                gap={4}
+            >
+                <ExpenseView />
+                <ExpenseView />
+            </Flex>
+        </Flex>
+    );
 }
---
+++ expense-tracker/src/components/summary/summary.jsx
@@ -0,0 +1,73 @@
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+
+export default function Summary() {
+    return (
+        <Box
+            p={6}
+            border={'1px solid'}
+            borderColor={'gray.100'}
+            overflow={'hidden'}
+            borderRadius={'10'}
+            background={'white'}
+            display={'flex'}
+            justifyContent={'center'}
+            alignItems={'center'}
+            w="full"
+        >
+            <Flex
+                w={'full'}
+                justifyContent={'space-between'}
+                alignItems={'center'}
+                flexDirection={{ base: 'column', lg: 'row' }}
+                gap={8}
+            >
+                {/* Summary Section */}
+                <Flex
+                    flex={'1'}
+                    flexDirection={'column'}
+                    alignItems={'center'}
+                    justifyContent={'center'}
+                    p={4}
+                >
+                    <Heading size={'md'} mb={'4'} color={'gray.600'}>
+                        Balance is 100
+                    </Heading>
+                    <Flex
+                        justifyContent={'space-between'}
+                        alignItems={'center'}
+                        bg={'gray.50'}
+                        w={'full'}
+                        border={'1px solid'}
+                        borderColor={'gray.100'}
+                        p={4}
+                        borderRadius={'10px'}
+                        flexDirection={'column'}
+                        gap={4}
+                    >
+                        <Flex flexDirection={'column'} alignItems={'center'}>
+                            <Heading color={'gray.700'}>$100</Heading>
+                            <Text color={'gray.600'}>Total Income</Text>
+                        </Flex>
+                        <Flex flexDirection={'column'} alignItems={'center'}>
+                            <Heading color={'gray.700'}>$100</Heading>
+                            <Text color={'gray.600'}>Total Expense</Text>
+                        </Flex>
+                    </Flex>
+                </Flex>
+
+                {/* Chart Section */}
+                <Box
+                    flex={'1'}
+                    display={'flex'}
+                    alignItems={'center'}
+                    justifyContent={'center'}
+                    height={'300px'}
+                    width={'300px'}
+                    p={4}
+                >
+                    <Heading>Chart</Heading>
+                </Box>
+            </Flex>
+        </Box>
+    );
+}
